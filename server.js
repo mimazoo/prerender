0a18e317727cd919d4de2a7fe7b0b43d3d21f3a7
Mitja Poljsak <mfsnoop@gmail.com>
2017-12-06 16:22:11 +0100

    Added parentheses

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
-var server = prerender(
+var server = prerender({
    pageDoneCheckInterval: 500,
    pageLoadTimeout: 20000,
    waitAfterLastRequest: 1000
-);
+});
 
 server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.blockResources());
