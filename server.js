916931a8e859a55b98ab6891fcc667b8f57734c9
Mitja Poljsak <mfsnoop@gmail.com>
2017-12-07 10:38:09 +0100

    Increased waitAfterLastRequest to 5 secs

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 var prerender = require('./lib');
 
 var server = prerender({
-   pageDoneCheckInterval: 500,
-   pageLoadTimeout: 20000,
-   waitAfterLastRequest: 1000
+   #pageDoneCheckInterval: 500,
+   #pageLoadTimeout: 20000,
+   waitAfterLastRequest: 5000
 });
 
 server.use(prerender.sendPrerenderHeader());
